f17160e43a8db5d40cc362e15601e18e0964ab6e
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-28 09:17:58 +0700

    buat seller

---
+++ app/Seller.php
@@ -0,0 +1,22 @@
+<?php
+namespace App;
+
+class Seller
+{
+    private $payment;
+
+    public function __construct(PaymentMethod $payment)
+    {
+        $this->payment = $payment;
+    }
+
+    public function setPayment(PaymentMethod $payment)
+    {
+        $this->payment = $payment;
+    }
+
+    public function cekPayment()
+    {
+        return $this->payment->cekIsi();
+    }
+}
