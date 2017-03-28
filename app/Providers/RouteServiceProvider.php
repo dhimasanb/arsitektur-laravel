dcaa2ab973cae3873a1738fd7b75de26c26a97a5
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-28 07:26:01 +0700

    tambah map custom routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapCustomRoutes();
         //
     }
 
@@ -69,5 +70,12 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    protected function mapCustomRoutes()
+    {
+      Route::middleware('web')
+           ->namespace($this->namespace)
+           ->group(base_path('routes/custom.php'));
     }
 }
