be5272ef506e5e07aa990d6e0738caedb5b7a915
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-29 00:19:36 +0700

    buat class kirimsurat implements contract

---
+++ app/KirimSurat.php
@@ -0,0 +1,11 @@
+<?php namespace App;
+
+use App\Contracts\MailListContract;
+
+class KirimSurat implements MailListContract {
+
+public function register($email)
+    {
+        return 'Mendaftarkan email ' . $email . ' ke server Kirim Surat.';
+    }
+}
