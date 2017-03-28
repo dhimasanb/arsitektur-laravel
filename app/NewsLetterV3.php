a47ca69cf578e378843fd986ebfff04e13fc3b9e
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-29 00:15:55 +0700

    buat class newsletterv3

---
+++ app/NewsLetterV3.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+
+use App\Contracts\MailListContract;
+
+class NewsLetterV3 {
+
+    protected $mailList;
+
+    public function __construct(MailListContract $mailList)
+    {
+        $this->mailList = $mailList;
+    }
+
+    public function register($email)
+    {
+        return $this->mailList->register($email);
+    }
+}
