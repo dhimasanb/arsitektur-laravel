0507dd67622a9e1b4dad3d34a1a4628fee438ee7
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-28 08:09:29 +0700

    buat markdown service provider

--- app/Providers/MarkdownServiceProvider.php
+++ app/Providers/MarkdownServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use League\CommonMark\CommonMarkConverter;
 
 class MarkdownServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class MarkdownServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->markdown = new CommonMarkConverter;
     }
 
     /**
@@ -23,6 +24,8 @@ class MarkdownServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('markdown', function() {
+          return $this->markdown;
+        });
     }
 }
